5e8c1d8edf0800cb163a0026dac2598e078696ee
Virgil <virgilhoover@gmail.com>
2020-12-07 04:22:46 -0700

    Fixed formatting and added auto open on log file

--- notifReminder.py
+++ notifReminder.py
@@ -1,22 +1,25 @@
-from datetime import datetime, timedelta
-from schedule import every, run_pending
-from win32com.client import Dispatch
-from os import getenv
 import tkinter as tk
-from operator import itemgetter
+import webbrowser as wb
+from datetime import datetime, timedelta
 from logging import getLogger, basicConfig, Formatter, WARNING
 from logging.handlers import RotatingFileHandler
+from operator import itemgetter
+from os import getenv
+from time import sleep
+
+from schedule import every, run_pending
+from win32com.client import Dispatch
 
 __name__ = 'notification_system'
 
 
 def logging(msg):
     access = 'a'
-    filename = getenv('UserProfile') + r'\notifications.log'
+    filename = getenv('AppData') + r'\notifications.log'
     size = 50 * 1024
     basicConfig(level=WARNING)
     file_handler = RotatingFileHandler(filename, access, maxBytes=size,
-                                       backupCount=2, encoding=None, delay=0)
+                                       backupCount=2, encoding=None, delay=False)
     file_format = Formatter('%(asctime)s - %(levelname)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
     file_handler.setLevel(WARNING)
@@ -24,6 +27,8 @@ def logging(msg):
     logger = getLogger(__name__)
     logger.addHandler(file_handler)
     logger.warning(msg)
+    sleep(1)
+    wb.open(filename)
 
 
 def send_alert_email(site_id, priority):
@@ -31,8 +36,9 @@ def send_alert_email(site_id, priority):
     msg = outlook.CreateItem(0)
     msg.to = 'someone@example.com'
     msg.Subject = site_id + ' ' + priority
-    msg.HTMLBody = '<html><body><p>Time for another update!\nIt is important to SLA\'s and the customer\nthat an ' \
-                   'update be sent promptly!</p></body></html>'
+    msg.HTMLBody = '<html><body><p>Time for another update!\nIt is important to' \
+                   'SLA\'s and the customer\nthat an update be sent promptly!' \
+                   '</p></body></html>'
     try:
         msg.Send()
     except Exception:
@@ -44,7 +50,7 @@ def check_alarms():
     for item in watching:
         if item[1] <= datetime.now():
             send_alert_email(item[0], item[2])
-            item[1] = item[1] + timedelta(minutes=15)
+            item[1] = item[1] + timedelta(minutes=30)
             display_watched()
 
 
@@ -115,25 +121,25 @@ def change_mode(tog=[0]):
     nfg = '#66FFFF'
     dbg = '#FFFFFF'
     dfg = '#000000'
-    
+
     if tog[0]:
-        root.option_add('*Backgroun', dbg)
+        root.option_add('*Background', dbg)
         root.option_add('*Foreground', dfg)
         root.configure(background=dbg, highlightbackground=dbg, highlightcolor=dfg)
-        
+
         for i in frames:
             i.configure(background=dbg, highlightbackground=dbg, highlightcolor=dfg)
-        
+
         for i in widgets:
             i.configure(background=dbg, foreground=dfg)
     else:
-        root.option_add('*Backgroun', nbg)
+        root.option_add('*Background', nbg)
         root.option_add('*Foreground', nfg)
         root.configure(background=nbg, highlightbackground=nbg, highlightcolor=nfg)
-        
+
         for i in frames:
             i.configure(background=nbg, highlightbackground=nbg, highlightcolor=nfg)
-            
+
         for i in widgets:
             i.configure(background=nbg, foreground=nfg)
 
@@ -152,42 +158,41 @@ email_recipient = tk.Variable(root)
 watching = []
 
 entry_frame = tk.Frame(root)
-entry_frame.grid(row=0, column=0)
 site_label = tk.Label(entry_frame, text='Site:')
-site_label.grid(row=0, column=0, sticky='e')
 site_entry = tk.Entry(entry_frame, textvariable=site_choice, width=8)
-site_entry.grid(row=0, column=1, padx=5)
 time_label = tk.Label(entry_frame, text='Time Down:')
-time_label.grid(row=0, column=2)
 time_entry = tk.Entry(entry_frame, textvariable=time_down_choice, width=8)
-time_entry.grid(row=0, column=3, padx=5)
 priority_choice.set('Priority')
 priority_c = tk.OptionMenu(entry_frame, priority_choice, 'P1', 'P2', 'P3')
 priority_c['highlightthickness'] = 0
+
+entry_frame.grid(row=0, column=0)
+site_label.grid(row=0, column=0, sticky='e')
+site_entry.grid(row=0, column=1, padx=5)
+time_label.grid(row=0, column=2)
+time_entry.grid(row=0, column=3, padx=5)
 priority_c.grid(row=0, column=4, padx=5)
 
 list_frame = tk.Frame(root)
-list_frame.grid(row=1, column=0)
 site_list = tk.Listbox(list_frame, width=50)
-site_list.grid(row=0, column=0, columnspan=3)
 add_button = tk.Button(list_frame, text='Add', borderwidth=0.5,
                        command=lambda: add_site(watching))
-add_button.grid(row=2, column=0, padx=5, pady=5)
 delete_button = tk.Button(list_frame, text='Remove', borderwidth=0.5,
                           command=remove_alarm)
-delete_button.grid(row=2, column=1, padx=5, pady=5)
-
 change_button = tk.Button(list_frame, text='Change', borderwidth=0.5,
                           command=change_mode)
+
+list_frame.grid(row=1, column=0)
+site_list.grid(row=0, column=0, columnspan=3)
+add_button.grid(row=2, column=0, padx=5, pady=5)
+delete_button.grid(row=2, column=1, padx=5, pady=5)
 change_button.grid(row=2, column=2, padx=5, pady=5)
 
 every(30).seconds.do(check_alarms)
-
 root.after(1000, run_schedule)
 site_entry.bind('<Tab>', focus_next)
 site_entry.bind('<Return>', add_site)
 time_entry.bind('<Return>', add_site)
 site_entry.focus()
 
-    
 root.mainloop()
